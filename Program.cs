4bf900f73cdb5d3ab6cca57b930b3830301a9212
dshettyy <dishanthshettyy@gmail.com>
2025-08-22 10:24:48 +0530

    Initial commit 1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("       Simple Calculator      ");
+        Console.WriteLine("/n Enter 1st Number :");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("/n Enter Second Number :");
+        int b = Convert.ToInt32(Console.ReadLine());    
+
+        Console.WriteLine("Enter The operation ( + , - , x , / , % ");
+        char oper = Convert.ToChar(Console.ReadLine());
+
+    switch (oper)
+            {
+
+            case '+':
+                Console.WriteLine("Addition : " + a + b);
+                break;
+
+            case '-':
+                Console.WriteLine("Addition : " + a - b);
+                break;
+
+            case '*':
+                Console.WriteLine("Multiplication :" + a * b);
+                break;
+
+            case '/':
+                Console.WriteLine("Division :" + a / b);
+                break;
+
+            case '%':
+                Console.WriteLine("Remainder : " + a % b);
+                break;
+
+            Default:
+                Console.WriteLine("Invalid operator");
+            }
+        }
+}
